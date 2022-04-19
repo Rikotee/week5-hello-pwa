4e3f9efcac967a45d22abb811c29a670bd88007a
Rikotee <teerek81@gmail.com>
2022-04-19 14:52:38 +0300

    fixed fontawesome

--- js/sw.js
+++ js/sw.js
@@ -5,32 +5,34 @@ var filesToCache = [
   './css/style.css',
   './js/main.js',
   './images/testImg.png',
-  './fonts/squarepeg-regular.woff2'
+  './assets',
+  './fonts/lato-black.woff2'
 ];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', (event) => {
-    event.waitUntil((async () => {
-          try {
-            const cache = await caches.open(cacheName);
-            return cache.addAll(filesToCache);
-          } catch (e) {
-            console.log(e.message);
-          }
-      })());
-  });
-  
+  event.waitUntil(
+    (async () => {
+      try {
+        const cache = await caches.open(cacheName);
+        return cache.addAll(filesToCache);
+      } catch (e) {
+        console.log(e.message);
+      }
+    })()
+  );
+});
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', (event) => {
-    console.log('ServiceWorker Fetch', event.request.url);
-    event.respondWith((async () => {
-          try {
-              const response = await caches.match(event.request);
-              return response || fetch(event.request);
-          } catch (e) {
-              console.log(e.message);
-          }
-      })());
-  });
-  
+  event.respondWith(
+    (async () => {
+      try {
+        const response = await caches.match(event.request);
+        return response || fetch(event.request);
+      } catch (e) {
+        console.log(e.message);
+      }
+    })()
+  );
+});
